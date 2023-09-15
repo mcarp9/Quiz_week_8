4323e4662fd925b5fdf62bf20688824ad632d03a
mcarp9 <uni.mattcarpio1@gmail.com>
2023-09-15 11:15:02 +1000

    Attempted more practice with climate.db

--- climate_q1.py
+++ climate_q1.py
@@ -16,22 +16,35 @@ cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData ORDER BY Year")
 
 
 
-#practice code for fetching the table name for climate.db
-#cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+# practice code for fetching the table name for climate.db
+# cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+# practice code for checking the fetching the values in the database
+# cursor.execute('SELECT * FROM your climate.db')
+# rowsValue = cursor.fetchall()
+# print (rowValue)
+
+
 tables = cursor.fetchall()
 
 #Practice for fetching the table name in the climate.db
 #for table in tables:
-    #print(table[0])
-
-
+#print(table[0])
 #print(tables)
 
-for table in tables:
+
+for table in tables: # populate the empty list created by rows.
     years.append(table[0])
     co2.append(table[1])
     temp.append(table[2])
 
+#to check if the empty list has been populated
+print(years)
+print(co2)
+print(temp)
+
+
+
+cursor.close()
 con.close()
 
 
@@ -46,5 +59,5 @@ plt.plot(years, temp, 'r*-')
 plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)")
 
-plt.savefig("q1.png")
+#plt.savefig("q1.png")
 plt.show()
