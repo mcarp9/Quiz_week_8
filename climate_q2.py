22b00010993148f98915281979754ae2a8c94122
mcarp9 <uni.mattcarpio1@gmail.com>
2023-09-16 10:37:23 +1000

    practiced different functions loading the csv file

--- climate_q2.py
+++ climate_q2.py
@@ -5,8 +5,11 @@ import pandas as pd
 
 # read the csv data
 climateData = pd.read_csv('climate.csv')
+#using anaconda prompt, cd to file location
+#show the data of years
 print(climateData.head())
 
+#extract the datas in the columns in order, (years, co2 and temp)
 years = climateData['Year']
 co2 = climateData['CO2']
 temp = climateData['Temperature']
@@ -24,6 +27,6 @@ plt.plot(years, temp, 'r*-')
 plt.ylabel("Temp (C)") 
 plt.xlabel("Year (decade)") 
 
-#savefig means save the figure and show the figure
+#savefig means save the figure and show means show the figure
 plt.savefig("q2.png")
 plt.show()
